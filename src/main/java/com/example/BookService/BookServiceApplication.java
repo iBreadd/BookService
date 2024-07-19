49690c096d8ba8c2de5d92f74b383148126c3225
iBreadd <ismailsunay0@gmail.com>
2024-07-19 16:53:49 +0300

    Some changes of configuration.

--- src/main/java/com/example/BookService/BookServiceApplication.java
+++ src/main/java/com/example/BookService/BookServiceApplication.java
@@ -1,9 +1,12 @@
 package com.example.BookService;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Book API", description = "book microservice"))
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/BookService/Entity/BookEntity.java
@@ -0,0 +1,30 @@
+package com.example.BookService.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stock;
+}
