fb82d8ce5c12d962b163298c7dad805d7420f7c8
iBreadd <ismailsunay0@gmail.com>
2024-10-25 11:51:15 +0300

    Fixed configuration between services.

--- src/main/java/com/example/BookService/config/WebClientConfig.java
+++ src/main/java/com/example/BookService/config/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @Value("${order.service.url:http://order-service:8080}")
+    @Value("${order.service.url}")
     private String orderServiceUrl;
 
     @Bean
